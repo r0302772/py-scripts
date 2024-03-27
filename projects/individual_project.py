cc0302e0c89d4eda032b5d95c1d2849450043a61
r0302772 <dennis.nijs@hotmail.com>
2024-03-27 11:01:35 +0100

    Individual project: step 2 - controlling an led with pwm

--- projects/individual_project.py
+++ projects/individual_project.py
@@ -1,37 +1,47 @@
 import wiringpi
 import time
 
-def ActivateADC ():
-    wiringpi.digitalWrite(pin_CS_adc, 0)       # Actived ADC using CS
+# Functions for ADC
+def ActivateADC():
+    wiringpi.digitalWrite(pin_CS_adc, 0)  # Activate ADC using CS
     time.sleep(0.000005)
 
 def DeactivateADC():
-    wiringpi.digitalWrite(pin_CS_adc, 1)       # Deactived ADC using CS
+    wiringpi.digitalWrite(pin_CS_adc, 1)  # Deactivate ADC using CS
     time.sleep(0.000005)
 
-def readadc(adcnum): 
-    if ((adcnum > 7) or (adcnum < 0)): 
-        return -1 
-    revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1,(8+adcnum)<<4,0]))
+def readadc(adcnum):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    revlen, recvData = wiringpi.wiringPiSPIDataRW(1, bytes([1, (8 + adcnum) << 4, 0]))
     time.sleep(0.000005)
-    adcout = ((recvData[1]&3) << 8) + recvData[2] 
-    return adcout  
+    adcout = ((recvData[1] & 3) << 8) + recvData[2]
+    return adcout
 
-#Setup
-pin_CS_adc = 16                                 #We will use w16 as CE, not the default pin w15!
-wiringpi.wiringPiSetup() 
-wiringpi.pinMode(pin_CS_adc, 1)                 # Set ce to mode 1 ( OUTPUT )
-wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0)  #(channel, port, speed, mode)
+# Function for LED control
+def controlLED(sig1, cnt):
+    wiringpi.softPwmWrite(sig1, cnt)
 
-#Main
+# Setup
+pin_CS_adc = 16  # We will use w16 as CE, not the default pin w15!
+pin_led_1 = 2  # Pin for LED 1
+
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(pin_CS_adc, 1)  # Set ce to mode 1 ( OUTPUT )
+wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0)  # (channel, port, speed, mode)
+wiringpi.softPwmCreate(pin_led_1, 0, 100)  # Set pin2 as a softPWM output
+
+# Main
 try:
     while True:
         ActivateADC()
-        tmp0 = readadc(0) # read channel 0
+        tmp0 = readadc(0)  # read channel 0
         DeactivateADC()
         print ("input0:",tmp0)
+        controlLED(pin_led_1, tmp0 // 10)  # Control LED brightness based on potentiometer
         time.sleep(0.2)
 
 except KeyboardInterrupt:
+    wiringpi.softPwmWrite(pin_led_1, 0)  # stop the PWM output
     DeactivateADC()
     print("\nProgram terminated")
