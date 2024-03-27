d689fe0a05968f9735903a5d2e998415f24d90c2
r0302772 <dennis.nijs@hotmail.com>
2024-03-27 12:40:01 +0100

    Individual project: Distance measurement

--- projects/individual_project.py
+++ projects/individual_project.py
@@ -27,14 +27,40 @@ def controlLEDs(sig1, sig2, cnt, min_brightness):
     else:
         wiringpi.digitalWrite(sig2, 0)  # Turn off the second LED
 
+# Function to measure distance
+def measure_distance():
+    # Send ultrasonic pulse
+    wiringpi.digitalWrite(TRIGGER_PIN, wiringpi.HIGH)
+    time.sleep(0.00001)  # 10 microseconds
+    wiringpi.digitalWrite(TRIGGER_PIN, wiringpi.LOW)
+
+    # Wait for the pulse to return
+    while wiringpi.digitalRead(ECHO_PIN) == wiringpi.LOW:
+        signal_high = time.time()
+
+    while wiringpi.digitalRead(ECHO_PIN) == wiringpi.HIGH:
+        signal_low = time.time()
+
+    # Calculate time passed
+    time_passed = signal_low - signal_high
+
+    # Calculate distance in centimeters
+    distance = time_passed * 17000
+
+    return distance
+
 # Setup
 pin_CS_adc = 16  # We will use w16 as CE, not the default pin w15!
 pin_led_1 = 2  # Pin for first LED
 pin_led_2 = 3  # Pin for second LED
+TRIGGER_PIN = 1  # Output pin for ultrasonic sensor
+ECHO_PIN = 4     # Input pin for ultrasonic sensor
 min_brightness = 20  # Set minimum brightness value
 wiringpi.wiringPiSetup()
 wiringpi.pinMode(pin_CS_adc, 1)  # Set ce to mode 1 ( OUTPUT )
 wiringpi.pinMode(pin_led_2, 1)  # Set pin3 to mode 1 ( OUTPUT )
+wiringpi.pinMode(TRIGGER_PIN, wiringpi.OUTPUT)
+wiringpi.pinMode(ECHO_PIN, wiringpi.INPUT)
 wiringpi.wiringPiSPISetupMode(1, 0, 500000, 0)  # (channel, port, speed, mode)
 wiringpi.softPwmCreate(pin_led_1, 0, 100)  # Set pin2 as a softPWM output
 
@@ -46,6 +72,8 @@ try:
         DeactivateADC()
         print ("input0:",tmp0)
         controlLEDs(pin_led_1, pin_led_2, tmp0 // 10, min_brightness)  # Control LED brightness based on potentiometer
+        distance = measure_distance()
+        print(f"Measured Distance = {round(distance, 2)} cm")
         time.sleep(0.2)
 
 except KeyboardInterrupt:
